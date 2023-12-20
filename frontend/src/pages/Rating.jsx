36554087b8cdd9f8a32351542fb094a099e296f8
Ryan Harang <ryh110.rh@gmail.com>
2023-12-20 11:06:30 -0800

    score select update

--- frontend/src/pages/Rating.jsx
+++ frontend/src/pages/Rating.jsx
@@ -85,33 +85,24 @@ export default function Rating() {
             className="field"
           />
           <label>Score</label>
-          <select
-            value={score}
-            className="select-score"
-            onChange={handleScoreChange}
-          >
-            <option value={0}>0</option>
-            <option value={0.5}>0.5</option>
-            <option value={1}>1</option>
-            <option value={1.5}>1.5</option>
-            <option value={2}>2</option>
-            <option value={2.5}>2.5</option>
-            <option value={3}>3</option>
-            <option value={3.5}>3.5</option>
-            <option value={4}>4</option>
-            <option value={4.5}>4.5</option>
-            <option value={5}>5</option>
-            <option value={5.5}>5.5</option>
-            <option value={6}>6</option>
-            <option value={6.5}>6.5</option>
-            <option value={7}>7</option>
-            <option value={7.5}>7.5</option>
-            <option value={8}>8</option>
-            <option value={8.5}>8.5</option>
-            <option value={9}>9</option>
-            <option value={9.5}>9.5</option>
-            <option value={10}>10</option>
-          </select>
+          <div className="custom-select">
+            <div className="score-display">
+              <span>{score}</span>
+            </div>
+            <div className="select-options">
+              {[...Array(21).keys()].map((value) => (
+                <div
+                  key={value}
+                  className="option"
+                  onClick={() =>
+                    handleScoreChange({ target: { value: value / 2 } })
+                  }
+                >
+                  {value / 2}
+                </div>
+              ))}
+            </div>
+          </div>
           <button type="submit" className="process">
             Submit
           </button>
