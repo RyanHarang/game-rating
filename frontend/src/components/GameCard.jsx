90fbbb369a537a051a6921c7ed38df2b46f4d00c
Ryan Harang <ryh110.rh@gmail.com>
2023-12-20 15:04:19 -0800

    Setup Site

--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import "../css/GameCard.css";
 
 const GameCard = ({ game }) => {
-  const imageUrl = game.imageUrl;
-
   return (
     <div className="card-container">
-      <h2 className="title">{game.title}</h2>
-      <p className="site">{game.site}</p>
+      <a className="site" href={game.site} target="_blank">
+        <h3 className="title">{game.title}</h3>
+      </a>
       <img
-        src={imageUrl}
+        src={game.imageUrl}
         alt={game.title}
         loading="lazy"
         className="game-image"
