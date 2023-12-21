08410180cdc7c057e6fc0c0a59fd075fec2ce7ea
Ryan Harang <ryh110.rh@gmail.com>
2023-12-21 12:30:31 -0800

    Added updating system to ratings to prevent duplicates

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -23,9 +23,16 @@ router.get("/game/:name", async (req, res) => {
 router.post("/rating", async (req, res) => {
   try {
     const { username, game, score } = req.body;
-    const newRating = new schemas.Rating({ username, game, score });
-    await newRating.save();
-    res.send("Rating sent");
+    const existingRating = await schemas.Rating.findOne({ username, game });
+    if (existingRating) {
+      existingRating.score = score;
+      await existingRating.save();
+      res.send("Rating updated");
+    } else {
+      const newRating = new schemas.Rating({ username, game, score });
+      await newRating.save();
+      res.send("Rating submitted");
+    }
   } catch (error) {
     console.error("Error saving rating:", error);
     res.status(500).send("Failed to send rating");
