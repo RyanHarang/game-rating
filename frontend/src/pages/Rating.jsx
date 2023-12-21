b09679c700a3dbedab9d69099c26637ba58bc811
Ryan Harang <ryh110.rh@gmail.com>
2023-12-20 17:46:57 -0800

    Added search parameters to the rating form.

--- frontend/src/pages/Rating.jsx
+++ frontend/src/pages/Rating.jsx
@@ -6,39 +6,45 @@ import axios from "axios";
 export default function Rating() {
   const { isGuest } = useAuth();
   const [username, setUsername] = useState("");
+  const [gameOptions, setGameOptions] = useState([]);
+  const [filteredOptions, setFilteredOptions] = useState([]);
   const [game, setGame] = useState("");
   const [score, setScore] = useState(5);
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    let processing = true;
-    //axiosFetchData(processing);
-    return () => {
-      processing = false;
+    const fetchGames = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/games");
+        const games = response.data.map((game) => game.title);
+        setGameOptions(games);
+        setFilteredOptions(games);
+      } catch (error) {
+        console.error("Error fetching games:", error);
+      }
     };
+    fetchGames();
   }, []);
 
   if (isGuest) {
-    // Redirect or show a message for guests
     return <p>You do not have access to the ratings page as a guest.</p>;
   }
 
   const handleScoreChange = (e) => {
-    // Update the score state when the user selects a value
     setScore(parseFloat(e.target.value));
   };
 
-  // eventually I'll need to use something similar to this to pull from a game list I think
-  // const axiosFetchData = async (processing) => {
-  //   await axios
-  //     .get("http://localhost:4000/users")
-  //     .then((res) => {
-  //       if (processing) {
-  //         setScore(res.data.score);
-  //       }
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
+  const handleGameChange = (e) => {
+    setGame(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    const filteredGames = gameOptions.filter((game) =>
+      game.toLowerCase().includes(searchTerm)
+    );
+    setFilteredOptions(filteredGames);
+  };
 
   const axiosPostData = async () => {
     const postData = {
@@ -46,9 +52,16 @@ export default function Rating() {
       game: game,
       score: score,
     };
-    await axios
-      .post("http://localhost:4000/rating", postData)
-      .then((res) => setMessage(<p className="success">{res.data}</p>));
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/rating",
+        postData
+      );
+      setMessage(<p className="success">{response.data}</p>);
+    } catch (error) {
+      console.error("Error posting rating:", error);
+      setMessage(<p className="error">Failed to submit rating</p>);
+    }
   };
 
   const handleSubmit = (e) => {
@@ -81,9 +94,29 @@ export default function Rating() {
             id="game"
             name="game"
             value={game}
-            onChange={(e) => setGame(e.target.value)}
+            onChange={handleGameChange}
+            onInput={handleSearch}
             className="field"
+            placeholder="Search for a game"
           />
+          {filteredOptions.length > 0 && (
+            <select
+              id="game-dropdown"
+              name="game-dropdown"
+              value={game}
+              onChange={handleGameChange}
+              className="field"
+            >
+              <option value="" disabled>
+                Select a game
+              </option>
+              {filteredOptions.map((game, index) => (
+                <option key={index} value={game}>
+                  {game}
+                </option>
+              ))}
+            </select>
+          )}
           <label>Score</label>
           <div className="custom-select">
             <div className="score-display">
