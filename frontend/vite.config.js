753d00f050980497d7ea5ee35e8ec9f7ad541465
Ryan Harang <ryh110.rh@gmail.com>
2023-12-22 14:43:19 -0800

    update config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,22 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default {
+export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom"],
-    },
-  },
-  optimizeDeps: {
-    include: ["react-router-dom"],
-  },
-};
+});
+
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default {
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ["react-router-dom"],
+//     },
+//   },
+//   optimizeDeps: {
+//     include: ["react-router-dom"],
+//   },
+// };
