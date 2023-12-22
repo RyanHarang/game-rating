354b79aa251a3d6faa4dfb29d3733b7020646246
Ryan Harang <ryh110.rh@gmail.com>
2023-12-22 12:59:40 -0800

    Added mobile hamburger menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,42 +5,54 @@ import Logo from "../../public/logo.png";
 
 export default function Header() {
   const { logout, isGuest, user } = useAuth();
+
   return (
-    <nav className="nav-bar">
-      <img className="logo" src={Logo} alt="dice-logo" />
-      {isGuest ? <p className="user">Guest</p> : <p className="user">{user}</p>}
-      {/* <div className="menu-icon">&#9776;</div> */}
-      <ul>
-        <li>
-          <Link to="/list" className="nav-link">
-            Games
-          </Link>
-        </li>
-        {!isGuest && (
+    <>
+      <nav className="nav-bar">
+        <div className="left-section">
+          <img className="logo" src={Logo} alt="dice-logo" />
+        </div>
+        {isGuest ? (
+          <p className="user">Guest</p>
+        ) : (
+          <p className="user">{user}</p>
+        )}
+        <input type="checkbox" id="menu-toggle" className="menu-toggle" />
+        <label htmlFor="menu-toggle" className="menu-icon">
+          &#9776;
+        </label>
+        <ul className="menu">
           <li>
-            <Link to="/add" className="nav-link">
-              Add
+            <Link to="/list" className="nav-link">
+              Games
             </Link>
           </li>
-        )}
-        {!isGuest && (
+          {!isGuest && (
+            <li>
+              <Link to="/add" className="nav-link">
+                Add
+              </Link>
+            </li>
+          )}
+          {!isGuest && (
+            <li>
+              <Link to="/rating" className="nav-link">
+                Rate
+              </Link>
+            </li>
+          )}
           <li>
-            <Link to="/rating" className="nav-link">
-              Rate
+            <Link to="/register" className="nav-link">
+              Register
             </Link>
           </li>
-        )}
-        <li>
-          <Link to="/register" className="nav-link">
-            Register
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="nav-link" onClick={() => logout()}>
-            Login
-          </Link>
-        </li>
-      </ul>
-    </nav>
+          <li>
+            <Link to="/" className="nav-link" onClick={() => logout()}>
+              Login
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
