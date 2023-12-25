48ee9baf15f11e51af6f8e74b003b0909d7145e6
Ryan Harang <ryh110.rh@gmail.com>
2023-12-25 10:46:39 -0800

    update footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
-      <p>Copyright &copy; 2023 Ryan Harang</p>
+      <p>Copyright &copy; 2024 Ryan Harang</p>
     </div>
   );
 }
