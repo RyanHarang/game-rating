bb4bd9387a2188aa7ff7962f40364b04b2058934
Ryan Harang <ryh110.rh@gmail.com>
2023-12-27 17:04:58 -0800

    delete extra

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import Header from "./Header";
 
 const Navigation = () => {
-  const navigate = useNavigate();
-
   const shouldDisplayHeader = () => {
     const currentPath = window.location.pathname;
     // List of paths where the header should not be displayed
