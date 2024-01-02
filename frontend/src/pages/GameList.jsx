2566271af9ecb2e3288830ab2b5ad18100505e42
Ryan Harang <ryh110.rh@gmail.com>
2024-01-02 11:38:14 -0800

    improved scroll positioning

--- frontend/src/pages/GameList.jsx
+++ frontend/src/pages/GameList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import GameCard from "../components/GameCard";
 import ScrollButton from "../components/ScrollButton";
 import axios from "axios";
@@ -8,45 +8,49 @@ export default function GameList() {
   const [games, setGames] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [scrollPosition, setScrollPosition] = useState(() => {
-    const storedScrollPosition = sessionStorage.getItem(
-      "gameListScrollPosition"
-    );
+    const storedScrollPosition = localStorage.getItem("gameListScrollPosition");
     return storedScrollPosition ? parseInt(storedScrollPosition, 10) : 0;
   });
 
   useEffect(() => {
-    sessionStorage.setItem("gameListScrollPosition", scrollPosition);
+    localStorage.setItem("gameListScrollPosition", scrollPosition.toString());
   }, [scrollPosition]);
+  const fetchGames = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/games?search=${searchTerm}`
+      );
+      setGames(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching games:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchGames = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:4000/games?search=${searchTerm}`
-        );
-        setGames(response.data);
-        setLoading(false);
-        const storedScrollPosition = sessionStorage.getItem(
-          "gameListScrollPosition"
-        );
-        window.scrollTo(0, storedScrollPosition || 0);
-      } catch (error) {
-        console.error("Error fetching games:", error);
-      }
-    };
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY);
-    };
-    window.addEventListener("scroll", handleScroll);
     fetchGames();
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
   }, [searchTerm]);
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
   };
+
+  const handleScroll = () => {
+    requestAnimationFrame(() => {
+      setScrollPosition(window.scrollY);
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo({ top: scrollPosition });
+  }, [games]);
 
   if (loading) {
     return <p className="loading">Loading...</p>;
